905c7e2c4b454f2415ce0e16b103797aecc78a26
Jonas Souza <jonasmzsouza@gmail.com>
2023-10-20 11:11:12 -0300

    project created

---
+++ src/main/java/me/dio/gof/DioSpringDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package me.dio.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringDesignPatternsApplication.class, args);
+	}
+
+}
