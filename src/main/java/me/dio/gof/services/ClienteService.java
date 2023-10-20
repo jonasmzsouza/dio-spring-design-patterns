12cc35c7104052dacf8e30e8d1b72528ccca8473
Jonas Souza <jonasmzsouza@gmail.com>
2023-10-20 12:25:13 -0300

    add clienteresource and clienteservice | viacepservice

---
+++ src/main/java/me/dio/gof/services/ClienteService.java
@@ -0,0 +1,24 @@
+package me.dio.gof.services;
+
+import me.dio.gof.entities.Cliente;
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ * 
+ * @author jonasmzsouza
+ */
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
