6279c74d9057d9d4be2c505907f68ec904edd782
Jonas Souza <jonasmzsouza@gmail.com>
2023-10-20 14:24:01 -0300

    fix generic error message

--- src/main/java/me/dio/gof/DioSpringDesignPatternsApplication.java
+++ src/main/java/me/dio/gof/DioSpringDesignPatternsApplication.java
@@ -2,8 +2,10 @@ package me.dio.gof;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DioSpringDesignPatternsApplication {
 
 	public static void main(String[] args) {
